012b558c35d11ef1c7608baeee619a49eca93542
Daniel Christopher <105526007+danchristopher02@users.noreply.github.com>
2025-10-30 20:13:20 -0400

    Create _keys.py

---
+++ _keys.py
@@ -0,0 +1,4 @@
+# Get your API Key: https://platform.openai.com/settings/organization/api-keys
+OPENAI_API_KEY = "{Place your OpenAI API key here}"
+LOG_ANALYTICS_WORKSPACE_ID = "{Place your Log Analytics Workspace ID here}"
+
